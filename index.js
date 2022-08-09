da4c38c0681130be1ffccd5e248cde1cf94db6d6
rukaiyaparween <rukaiyaparween58@gmail.com>
2022-08-09 21:07:32 +0530

    test change

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ database.on('error', (error) => {
 })
 
 database.once('connected', () => {
-    console.log('Database Connected');
+    console.log('Database Connected :)');
 })
 const app = express();
 
